6c6eb16587ea9258001b5daead6785862b3fe220
cspy <cspy2117@163.com>
2023-10-17 11:03:58 +0800

    初始化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,50 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import 'antd/dist/reset.css';
+import {Button, ConfigProvider, Form, Input} from "antd";
+import {LoginForm, ProFormText} from "@ant-design/pro-form";
+import {LockOutlined, UserOutlined} from "@ant-design/icons";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <main>
+            <ConfigProvider theme={{
+                token: {
+                    borderRadius: 2,
+                    colorPrimary: "#0053a0"
+                }
+            }}>
+                <Form>
+                    <Form.Item>
+                        <Input/>
+                    </Form.Item>
+                </Form>
+                <Button type={"primary"}>测试按钮</Button>
+                <LoginForm title={"测试样式"}>
+                    <ProFormText
+                        name="username"
+                        fieldProps={{
+                            size: 'large',
+                            prefix: <UserOutlined className={'prefixIcon'}/>,
+                        }}
+                        placeholder={'用户名'}
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入用户名!',
+                            },
+                        ]}
+                    />
+                    <ProFormText.Password
+                        name="password"
+                        fieldProps={{
+                            size: 'large',
+                            prefix: <LockOutlined className={'prefixIcon'}/>,
+                        }}
+                        placeholder={'密码'}
+                        rules={[]}
+                    />
+                </LoginForm>
+            </ConfigProvider>
+        </main>
+    )
 }
